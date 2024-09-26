91ba4ebc8fe4c1143f84096dac90a3e6449a9257
William Blanchet Lafrenière <willclash@outlook.com>
2024-09-25 22:41:03 -0400

    cleanup + basic testing

--- src/GameConfig.py
+++ src/GameConfig.py
@@ -1,6 +1,9 @@
+from pygame import Vector2
 
 
-class GameConfig():
-    SCREEN_DIMENSION = (800, 600)
+class GameConfig:
+    SCREEN_DIMENSION = Vector2(800, 600)
     WINDOW_NAME = "Flappy Brain"
-    
+    REFRESH_RATE = 60
+    SCROLL_SPEED = 6
+    GROUND_SPACE = 50
--- src/GameManager.py
+++ src/GameManager.py
@@ -1,57 +1,67 @@
 import pygame
 import pygame.gfxdraw
-import math
+from pygame import Vector2
+
+from src.Bird import Bird
 from src.GameConfig import GameConfig
+
+
 class GameManager:
-    
+
     def __init__(self):
-        self.init_variables()
+        self.dt = 0
+        self.running = True
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode(Vector2(GameConfig.SCREEN_DIMENSION.x, GameConfig.SCREEN_DIMENSION.y +GameConfig.GROUND_SPACE))
+        self.scroll = 0
+        self.scroll_speed = GameConfig.SCROLL_SPEED
+        self.bg_img = pygame.transform.scale(pygame.image.load('assets/bg.png'), GameConfig.SCREEN_DIMENSION)
+        self.ground_img = pygame.image.load('assets/ground.png')
+        self.bird = Bird()
         self.setup_pygame()
-    
-    def init_variables(self):
-        pass
-    
-    def setup_pygame(self):
+
+    @staticmethod
+    def setup_pygame():
         pygame.init()
         pygame.display.set_caption(GameConfig.WINDOW_NAME)
-        self.screen = pygame.display.set_mode(GameConfig.SCREEN_DIMENSION)
-        self.clock = pygame.time.Clock()
-        self.running = True
-        self.dt = 0
-    
+
     def start_application(self):
         print('Application Flappy_EEG starting...')
         self.game_loop()
-    
+
     def game_loop(self):
         # Main game loop
         while self.running:
-            
+
             # poll for events
-            # pygame.QUIT event means the user clicked X to close your window
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-            
+
             # clear the screen buffer with a color
-            self.screen.fill("green")
-            
-            ###
-            # The game render take place here
-            ###
-            t = pygame.time.get_ticks()
-            longueur_onde = 500
-            color = (255, 0, 255)
-            x = self.screen.get_width()/2 + math.sin(t/longueur_onde) * 100
-            y = self.screen.get_height()/2 + math.cos(t/longueur_onde) * 100
-            position = pygame.Vector2(x,y)
-            pygame.gfxdraw.aacircle(self.screen, int(x),int(y), 30,color)
-            pygame.gfxdraw.filled_circle(self.screen, int(x),int(y), 30, color)
-            
+            self.screen.blit(self.bg_img, (0, 0))
+
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_SPACE]:
+                self.bird.jump(self.dt)
+
+            if self.bird.position.y <= 0:
+                self.bird.position.y = 0
+
+            self.bird.position.y += self.dt * 100
+
+            self.bird.draw(self.screen)
+
+            self.screen.blit(self.ground_img, (self.scroll, GameConfig.SCREEN_DIMENSION.y))
+
+            self.scroll += self.scroll_speed
+            if abs(self.scroll) > 35:
+                self.scroll = 0
+
             # flip() to make the drawing appear on screen
             pygame.display.flip()
-            
-            # clock to limit loop to 60 frames/second
-            self.dt = self.clock.tick(60) / 1000
-        
-        pygame.quit
+
+            # clock to limit loop to the refresh rate
+            self.dt = self.clock.tick(GameConfig.REFRESH_RATE) / 1000
+
+        pygame.quit()
