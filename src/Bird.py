707bf79139466349054081da8edd29c7b316ec47
Louis-Etienne Messier <lemes3@ulaval.ca>
2024-09-30 10:47:12 -0400

    modifs de bird selon la docu de pygame

--- src/Bird.py
+++ src/Bird.py
@@ -1,16 +1,54 @@
 import pygame
+from src.util import load_image
 
 from src.GameConfig import GameConfig
 
 
-class Bird:
-    def __init__(self):
-        self.collisions = pygame.Vector2(0, 0)
-        self.position = pygame.Vector2(GameConfig.SCREEN_DIMENSION.x / 2, GameConfig.SCREEN_DIMENSION.y / 2)
+class Bird(pygame.sprite.Sprite):
+    def __init__(self, screen:pygame.Surface):
+        pygame.sprite.Sprite.__init__(self)
+        self.init_variables(screen)
         self.velocity = pygame.Vector2(0, 0)
+    
+    def init_variables(self, screen:pygame.Surface):
+        self.const_image, self.const_rect = load_image("assets/nath_head_only.png", -1)
+        self.const_rect.center = (screen.get_width()/2,
+                                  screen.get_height()/2)
+        self.image =  self.const_image
+        self.rect = self.const_rect
+
+    def rotate_around_center(self, image, angle, x,y):
+        rotated_image = pygame.transform.rotate(image, angle)
+        new_rect = rotated_image.get_rect(center=image.get_rect(center=(x,y)).center)
+        return rotated_image, new_rect
+    
+    def update(self, dt):
+        
+        # position update
+        self.gravity(dt)
+        self.const_rect.center += self.velocity * dt
+
+        # limit the character to the screen limit
+        if self.const_rect.centery > GameConfig.SCREEN_DIMENSION.y:
+            self.const_rect.centery = GameConfig.SCREEN_DIMENSION.y
+            self.velocity.y = 0
+
+        # rotation update, if not crashed on the ground
+        if self.const_rect.centery < GameConfig.SCREEN_DIMENSION.y:
+            speed = 0.1
+            angle = pygame.time.get_ticks() * speed
+            self.image, self.rect = self.rotate_around_center(
+                self.const_image,
+                angle,
+                self.const_rect.centerx,
+                self.const_rect.centery 
+            )
 
     def jump(self, dt):
-        self.position.y -= 1000 * dt
+        jump_force = 300
+        self.velocity.y = -jump_force
 
-    def draw(self, screen):
-        pygame.draw.circle(screen, "blue", self.position, 40)
+    def gravity(self, dt):
+        gravity_force = 800
+        self.velocity.y += gravity_force * dt
+
---
+++ src/Game.py
@@ -0,0 +1,46 @@
+from src.Bird import Bird
+from src.InputManager import InputManager
+from src.GameConfig import GameConfig
+from src.util import load_image
+import pygame
+
+
+class Game:
+    def __init__(self, screen:pygame.Surface):
+        self.init_variables(screen)
+
+    def init_variables(self, screen:pygame.Surface):
+        self.score = 0
+        self.bird = Bird(screen)
+        self.scroll = 0
+        self.scroll_speed = GameConfig.SCROLL_SPEED
+        self.bg_img = pygame.transform.scale(pygame.image.load('assets/bg.png'), GameConfig.SCREEN_DIMENSION)
+        self.bg_img, _ = load_image('assets/bg.png', resize=GameConfig.SCREEN_DIMENSION)
+        self.ground_img, _ = load_image('assets/ground.png')
+
+        # adds all sub-objects to the render group
+        self.group = pygame.sprite.RenderPlain((self.bird))
+    
+    def click():
+        pass
+
+    def add_score():
+        pass
+
+    def draw(self, screen):
+        screen.blit(self.ground_img, (self.scroll, GameConfig.SCREEN_DIMENSION.y))
+        self.group.draw(screen)
+
+    def update(self, dt):
+        # jump if space is down
+        if InputManager.is_jump_down():
+            self.bird.jump(dt)
+
+        # scroll the background
+        self.scroll += self.scroll_speed
+        if abs(self.scroll) > 35:
+            self.scroll = 0
+  
+        # update it's sub-objects
+        self.group.update(dt)
+        
