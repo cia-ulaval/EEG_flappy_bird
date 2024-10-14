f87d0ffb909f90b4d5810c4256d41243a70e15a2
QuentinClyy <nyxoloswag@gmail.com>
2024-10-14 12:22:55 -0400

    collision height between ground and bird is now relative to ground space

--- src/Bird.py
+++ src/Bird.py
@@ -33,8 +33,8 @@ class Bird(pygame.sprite.Sprite):
         self.const_rect.center += self.velocity * dt
 
         # limit the character to the screen limit
-        if self.const_rect.centery > GameConfig.SCREEN_DIMENSION.y - 20:
-            self.const_rect.centery = GameConfig.SCREEN_DIMENSION.y - 20
+        if self.const_rect.centery > GameConfig.SCREEN_DIMENSION.y - GameConfig.GROUND_SPACE/2:
+            self.const_rect.centery = GameConfig.SCREEN_DIMENSION.y - GameConfig.GROUND_SPACE/2
             self.velocity.y = 0
 
         # change back to default sprite if finished jump
@@ -68,7 +68,7 @@ class Bird(pygame.sprite.Sprite):
 
     def crashed(self):
         crashed = False
-        if self.const_rect.centery == GameConfig.SCREEN_DIMENSION.y - 20:
+        if self.const_rect.centery == GameConfig.SCREEN_DIMENSION.y - GameConfig.GROUND_SPACE/2:
             crashed = True
         return crashed
 
