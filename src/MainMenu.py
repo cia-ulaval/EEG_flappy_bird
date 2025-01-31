434c43158b38958a188a843fa00028ec07fd15ca
VillyGH <willclash@outlook.com>
2025-01-31 14:42:05 -0500

    Update MainMenu.py

--- src/MainMenu.py
+++ src/MainMenu.py
@@ -1,25 +1,20 @@
 import pygame_menu as pm
-import json
 import pygame
-from math import log10
-
 import pygame_menu.font
-from src.InputManager import InputManager
+
 from src.util import load_image
 from src.GameConfig import GameConfig
 from src.LEVELS import Levels
 
 
-def update_level():
-    if InputManager.echap_pressed:
-        return Levels.MENU
-    else:
-        return Levels.SCOREBOARD
+def update_level(level:Levels):
+    return level
 
 class MainMenu:
     def __init__(self, screen:pygame.Surface):
         self.MENU_FONT_TILE_SIZE = 40
         self.MENU_FONT_P_SIZE = 20
+        self.screen = screen
         self.theme = pm.themes.THEME_SOLARIZED.copy()
         self.bg_img = pygame.transform.scale(pygame.image.load('assets/bg.png'), GameConfig.SCREEN_DIMENSION)
         self.menu = pm.Menu(width=GameConfig.SCREEN_DIMENSION[0] - 200,
@@ -41,13 +36,13 @@ class MainMenu:
     def create_menu(self):
         self.menu.set_relative_position(50, 55)
         self.menu.add.label(title="Flappy Bird EEG\n\n", font_size=self.MENU_FONT_TILE_SIZE, font_color=GameConfig.FONT_COLOR,
-                            font_name=pygame_menu.font.FONT_8BIT)
-        self.menu.add.label(title="Commencer\n\n", font_size=self.MENU_FONT_P_SIZE, font_color=GameConfig.FONT_COLOR,
-                            font_name=pygame_menu.font.FONT_8BIT)
-        self.menu.add.label(title="Options\n\n", font_size=self.MENU_FONT_P_SIZE, font_color=GameConfig.FONT_COLOR,
-                            font_name=pygame_menu.font.FONT_8BIT)
-        self.menu.add.label(title="Options\n\n", font_size=self.MENU_FONT_P_SIZE, font_color=GameConfig.FONT_COLOR,
-                            font_name=pygame_menu.font.FONT_8BIT)
+                            font_name=pygame_menu.font.FONT_8BIT, )
+        self.menu.add.button(title="Commencer\n\n", font_size=self.MENU_FONT_P_SIZE, font_color=GameConfig.FONT_COLOR,
+                            font_name=pygame_menu.font.FONT_8BIT, onselect=update_level(Levels.GAME))
+        self.menu.add.button(title="Options\n\n", font_size=self.MENU_FONT_P_SIZE, font_color=GameConfig.FONT_COLOR,
+                            font_name=pygame_menu.font.FONT_8BIT, onselect=update_level(Levels.CONFIG))
+        self.menu.add.button(title="Quitter\n\n", font_size=self.MENU_FONT_P_SIZE, font_color=GameConfig.FONT_COLOR,
+                            font_name=pygame_menu.font.FONT_8BIT, onselect=pygame.quit())
 
     def draw(self, screen):
         screen.blit(self.bg_img, (0, 0))
